6d6a6790893b5f4dfb9ab9a3564c69ea5069976f
Jesse Gallardo <31861197+JesseGallardo@users.noreply.github.com>
2023-06-21 21:44:03 -0400

    Create Home Page (#2)
    * Created component for home page
    
    * Created component for sets page
    
    * Created component for card pool
    
    * Created component for credits page
    
    * Created component for discord
    
    * Created component for page not found
    
    * Edited title for pages
    
    * Added components for redirects
    
    * Added bootstrap to app
    
    * Added bootstrap and jquery
    
    * initial draft of nav bar completed
    
    * Removed Discord page and components
    
    * Removed Credits page and components
    
    * Created global background color
    
    * Initial draft of home page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { SetsComponent } from './sets/sets.component';
+import { CardPoolComponent } from './card-pool/card-pool.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetsComponent,
+    CardPoolComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'sets', component: SetsComponent},
+      {path: 'card-pool', component: CardPoolComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card-pool/card-pool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pool',
+  templateUrl: './card-pool.component.html',
+  styleUrls: ['./card-pool.component.css']
+})
+export class CardPoolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
